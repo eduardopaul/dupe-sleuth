fc458cfc063fc4c8ac36eabf91a76bb3e623df28
Eduardo Paul <ecpaul@pm.me>
2025-12-08 17:13:33 -0300

    Added argparse.

--- dupsleuth.py
+++ dupsleuth.py
@@ -1,21 +1,39 @@
+from argparse import ArgumentParser
 from collections import defaultdict
 from hashlib import file_digest
 from pathlib import Path
 
 
-p = Path('files/')
+parser = ArgumentParser()
 
-d = defaultdict(list)
-for step in p.walk():
-    root = step[0]
-    for file_name in step[2]:
-        f_path = root/file_name
-        with open(f_path, 'rb') as f:
-            hash = file_digest(f, 'md5').hexdigest()
-            d[hash].append(str(f_path)) 
+parser.add_argument(
+    "dir",
+    help="Root directory the search should be performed in.",
+    type=Path,
+)
 
-repeated_files = [f_paths for hash, f_paths in d.items() if len(f_paths) > 1]
+args = parser.parse_args()
 
-for line in repeated_files:
-    print(line)
+p = args.dir
 
+if p.exists():
+
+    d = defaultdict(list)
+    for step in p.walk():
+        root = step[0]
+        for file_name in step[2]:
+            f_path = root/file_name
+            with open(f_path, "rb") as f:
+                hash = file_digest(f, "md5").hexdigest()
+                d[hash].append(str(f_path)) 
+
+    repeated_files = [f_paths for hash, f_paths in d.items() if len(f_paths) > 1]
+
+    output_file = Path("repeated_files.txt")
+    with open(output_file, "w") as f:
+        f.write("Each line is a set of identical files.\n")
+        for line in repeated_files:
+            f.write(str(line) + "\n")
+
+else:
+    print(f"The directory \"{p}\" does not exist.")
