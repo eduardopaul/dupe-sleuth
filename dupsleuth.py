20d6d4c714c2c91c81e05e2f1197978fcfd5aecb
Eduardo Paul <ecpaul@pm.me>
2025-12-09 10:32:58 -0300

    Added size checking before hashing.

--- dupsleuth.py
+++ dupsleuth.py
@@ -18,22 +18,38 @@ p = args.dir
 
 if p.exists():
 
-    d = defaultdict(list)
+    # First group by file size.
+    size_dict = defaultdict(list)
     for step in p.walk():
         root = step[0]
         for file_name in step[2]:
-            f_path = root/file_name
-            with open(f_path, "rb") as f:
-                hash = file_digest(f, "md5").hexdigest()
-                d[hash].append(str(f_path)) 
+            file_path = root/file_name
+            file_size = file_path.stat().st_size
+            size_dict[file_size].append(file_path)
 
-    repeated_files = [f_paths for hash, f_paths in d.items() if len(f_paths) > 1]
+    # Then check duplicates by hash.
+    duplicate_files = defaultdict(list)
+    for size, file_list in size_dict.items():
+        if len(file_list) > 1:
+            for file_path in file_list:
+                with open(file_path, "rb") as f:
+                    hash = file_digest(f, "md5").hexdigest()
+                    duplicate_files[hash].append(str(file_path.relative_to(p))) 
+
+    repeated_files = [
+        file_paths
+        for hash, file_paths in duplicate_files.items()
+        if len(file_paths) > 1
+    ]
 
     output_file = Path("repeated_files.txt")
     with open(output_file, "w") as f:
-        f.write("Each line is a set of identical files.\n")
+        f.write("Each group is a set of tentative identical files.\n\n")
         for line in repeated_files:
-            f.write(str(line) + "\n")
+            for file in line:
+                f.write(str(file) + "\n")
+            f.write("\n")
 
 else:
     print(f"The directory \"{p}\" does not exist.")
+
